fcc3655edf2dbc7335e949f0b9771d2209083119
Rahul <mouryarahul543@gmail.com>
2025-07-21 20:50:05 +0530

    some fixed

--- client/src/pages/Ticketpage.jsx
+++ client/src/pages/Ticketpage.jsx
@@ -56,7 +56,7 @@ const TicketPage = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       {/* Header */}
-      <Header />
+      <Header  className=" z-50"/>
 
       {/* Main Content */}
       <main className="flex-1 max-w-7xl mx-auto w-full p-4 sm:p-6 lg:p-8 grid grid-cols-[2fr_1fr] md:grid-cols-1 gap-8">
