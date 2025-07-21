fa04e002724e0199d11dc4d32fe03a3451e6f975
Rahul <mouryarahul543@gmail.com>
2025-07-22 02:01:40 +0530

    added some feat

--- client/src/pages/SeatBook.jsx
+++ client/src/pages/SeatBook.jsx
@@ -222,20 +222,20 @@ const SeatBook = () => {
       <div
           className={`self-stretch [background:linear-gradient(100.3deg,_#114f8b,_#2a9ad7)] flex flex-col items-start justify-start text-left text-42xl text-white font-baloo-bhai`}
         >
-      <div className="self-stretch relative h-[362px] sm:h-[480px]">
-        <div className="absolute h-full w-full top-[0px] right-[0px] bottom-[0px] left-[0px] [background:linear-gradient(78.78deg,_#114f8b_5.2%,_#2a9ad7)]" />
+      <div className=" z-0 self-stretch relative h-[350px]">
+        <div className="absolute w-full top-[0px] right-[0px] left-[0px] [background:linear-gradient(78.78deg,_#114f8b_5.2%,_#2a9ad7)] h-[350px]" />
         <img
-          className="absolute h-full w-full top-[0.4px] right-[0px] bottom-[-0.4px] left-[0px] max-w-full overflow-hidden max-h-full object-cover"
+          className="absolute w-full top-[0.4px] right-[0px] left-[0px] max-w-full overflow-hidden h-[350px] object-cover"
           alt=""
-          src="/banner-background@2x.png"
+          src="/banner--background@2x.png"
         />
-        <div className="absolute w-full top-[calc(50%_-_96.5px)] right-[0px] left-[0px] flex flex-col items-center justify-center py-0 px-20 box-border gap-[20px] md:pl-[30px] md:pr-[30px] md:box-border md:w-full md:top-[calc(44%_-_96.5px_-_0px)] sm:w-full sm:top-[calc(32%_-_96.5px_-_0px)]">
+        <div className="absolute top-[0px] left-[0px] w-full h-[350px] flex flex-col items-center justify-center py-0 px-[140px] box-border gap-[43px] md:pl-[30px] md:pr-[30px] md:box-border">
           <div className="self-stretch flex flex-col items-center justify-start gap-[5px]">
-            <div className="self-stretch sm:text-23xl sm:leading-[48px]">Select Your Seat!!!</div>
-            <div className="self-stretch text-5xl leading-[32px] font-roboto">
+            <div className="self-stretch relative leading-[67px] sm:text-23xl sm:leading-[48px]">Select Your Seat !!!</div>
+            <div className="self-stretch relative text-5xl leading-[32px] font-roboto">
               Choose your preferred seat for your journey
             </div>
-          </div>
+        </div>
         </div>
        </div>
        </div>
